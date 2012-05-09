4ffcafdaca5aee3fa025c27b1a6817219b448983
root <root@fubar-100-22.dmz.caret.local>
2012-05-09 17:23:37 +0100

    Updated login.php to fail noisily if auth is broken.

--- htdocs/php/login.php
+++ htdocs/php/login.php
@@ -2,7 +2,15 @@
 
 require_once 'auth.php';
 
-session_start();
-set_user($_SERVER['REMOTE_USER']);
-
-header("Location: ../list.html");
+if(!$_SERVER['REMOTE_USER']) {
+	header("HTTP/1.0 500 Server Error");
+	?>
+	<h1>Auth Failure</h1>
+	<p>Raven auth does not seem to be working</p>
+	<?php
+}
+else {
+	session_start();
+	set_user($_SERVER['REMOTE_USER']);
+	header("Location: ../list.html");
+}
